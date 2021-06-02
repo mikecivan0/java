1ec708dab7c09acd42663551567cd96cf7cd23b9
mikecivan0 <mikecivan0@gmail.com>
2021-06-03 01:05:06 +0200

    deklariranje varijabli primitivnog tipa

---
+++ src/edunova/K02_PrimitivniTipovi.java
@@ -0,0 +1,24 @@
+package edunova;
+
+public class K02_PrimitivniTipovi {
+
+	public static void main(String[] args) {
+
+		boolean aktivan = true;
+		
+		byte brojDo127 = 6;
+		
+		short veciBroj = 258;
+		
+		int broj = 65444;
+		
+		long velikiBroj = 6546468468L;
+		
+		float decimalniManji = 3.14f;
+		
+		double velikiDecimalni = 3e10;
+		
+		char znak = 'E';
+	
+	}
+}
