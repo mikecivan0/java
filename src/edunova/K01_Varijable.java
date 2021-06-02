e4982d18436b302f5911ea9718a76f6b85b31b1b
mikecivan0 <mikecivan0@gmail.com>
2021-06-03 00:55:29 +0200

    deklariranje varijabli

---
+++ src/edunova/K01_Varijable.java
@@ -0,0 +1,35 @@
+package edunova;
+
+public class K01_Varijable {
+	public static void main(String[] args) {
+		// deklariranje varijable tipa int i dodjeljivanje vrijednosti
+
+		int i = 1;
+
+		// deklariranje varijable
+		int t;
+
+		// dodavanje vrijednosti
+		t = 7;
+
+		// korištenje varijable
+		System.out.println(t);
+
+		// komentari
+		// za pojedinaèni
+		// red
+
+		/*
+		 * komentari za cijeli blok
+		 * 
+		 * 
+		 */
+
+		// deklariranje više varijabli odjednom
+
+		int k, l, m;
+
+		int v = 2, p = 0;
+	}
+
+}
