f0d72f194b5355591151a81196efc2027f4a9494
mikecivan0 <mikecivan0@gmail.com>
2021-06-03 14:46:22 +0200

    objasnjenje brojevne kruznice

---
+++ src/edunova/K04_BrojevnaKruznica.java
@@ -0,0 +1,16 @@
+package edunova;
+
+public class K04_BrojevnaKruznica {
+	public static void main(String[] args) {
+
+		//zadavanje najveæe int vrijednosti
+		
+		int najveci = Integer.MAX_VALUE;
+		
+		//uveæavanje najveæe vrijednosti za 1
+		najveci++;
+		
+		System.out.println(najveci);
+		//output -2147483648
+	}
+}
